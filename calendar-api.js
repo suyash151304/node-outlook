4dc1a3df049601cf5ce12a0bd77719a629f01883
suyash jain <suyash@blitzcodesys.com>
2019-08-26 20:16:22 +0530

    accept reject events

--- calendar-api.js
+++ calendar-api.js
@@ -610,5 +610,176 @@ module.exports = {
         }
       }
     });
-  }
+  },
+
+
+  /**
+   * accept response to event.
+   *
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.eventId {string} The Id of the event.
+   * @param parameters.update {object} The JSON-serializable update payload
+   * @param [parameters.useMe] {boolean} If true, use the `/Me` segment instead of the `/Users/<email>` segment. This parameter defaults to false and is ignored if the `parameters.user.email` parameter isn't provided (the `/Me` segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the `/Me` segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [parameters.odataParams] {object} An object containing key/value pairs representing OData query parameters. See [Use OData query parameters]{@link https://msdn.microsoft.com/office/office365/APi/complex-types-for-mail-contacts-calendar#UseODataqueryparameters} for details.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   *
+   * @example var outlook = require('node-outlook');
+   *
+   * // Set the API endpoint to use the v2.0 endpoint
+   * outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+   *
+   * // This is the oAuth token
+   * var token = 'eyJ0eXAiOiJKV1Q...';
+   *
+   * // The Id property of the event to update. This could be
+   * // from a previous call to getEvents
+   * var eventId = 'AAMkADVhYTYwNzk...';
+   *
+   * // Update the location
+   * var update = {
+   *   Location: {
+   *     DisplayName: 'Conference Room 2'
+   *   }
+   * };
+   *
+   * // Pass the user's email address
+   * var userInfo = {
+   *   email: 'sarad@contoso.com'
+   * };
+   *
+   * outlook.calendar.acceptEvent({token: token, eventId: eventId, update: update, user: userInfo},
+   *   function(error, result){
+   *     if (error) {
+   *       console.log('updateEvent returned an error: ' + error);
+   *     }
+   *     else if (result) {
+   *       console.log(JSON.stringify(result, null, 2));
+   *     }
+   *   });
+   */
+  acceptEvent: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+
+    var requestUrl = base.apiEndpoint() + userSpec + '/Events/' + parameters.eventId + '/accept';
+
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user,
+      payload: parameters.update,
+      method: 'POST'
+    };
+
+    if (parameters.odataParams !== undefined) {
+      apiOptions['query'] = parameters.odataParams;
+    }
+
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 200) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+
+
+  
+  /**
+   * reject response to event.
+   *
+   * @param parameters {object} An object containing all of the relevant parameters. Possible values:
+   * @param parameters.token {string} The access token.
+   * @param parameters.eventId {string} The Id of the event.
+   * @param parameters.update {object} The JSON-serializable update payload
+   * @param [parameters.useMe] {boolean} If true, use the `/Me` segment instead of the `/Users/<email>` segment. This parameter defaults to false and is ignored if the `parameters.user.email` parameter isn't provided (the `/Me` segment is always used in this case).
+   * @param [parameters.user.email] {string} The SMTP address of the user. If absent, the `/Me` segment is used in the API URL.
+   * @param [parameters.user.timezone] {string} The timezone of the user.
+   * @param [parameters.odataParams] {object} An object containing key/value pairs representing OData query parameters. See [Use OData query parameters]{@link https://msdn.microsoft.com/office/office365/APi/complex-types-for-mail-contacts-calendar#UseODataqueryparameters} for details.
+   * @param [callback] {function} A callback function that is called when the function completes. It should have the signature `function (error, result)`.
+   *
+   * @example var outlook = require('node-outlook');
+   *
+   * // Set the API endpoint to use the v2.0 endpoint
+   * outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+   *
+   * // This is the oAuth token
+   * var token = 'eyJ0eXAiOiJKV1Q...';
+   *
+   * // The Id property of the event to update. This could be
+   * // from a previous call to getEvents
+   * var eventId = 'AAMkADVhYTYwNzk...';
+   *
+   * // Update the location
+   * var update = {
+   *   Location: {
+   *     DisplayName: 'Conference Room 2'
+   *   }
+   * };
+   *
+   * // Pass the user's email address
+   * var userInfo = {
+   *   email: 'sarad@contoso.com'
+   * };
+   *
+   * outlook.calendar.rejectEvent({token: token, eventId: eventId, update: update, user: userInfo},
+   *   function(error, result){
+   *     if (error) {
+   *       console.log('updateEvent returned an error: ' + error);
+   *     }
+   *     else if (result) {
+   *       console.log(JSON.stringify(result, null, 2));
+   *     }
+   *   });
+   */
+  rejectEvent: function(parameters, callback) {
+    var userSpec = utilities.getUserSegment(parameters);
+
+    var requestUrl = base.apiEndpoint() + userSpec + '/Events/' + parameters.eventId + '/decline';
+
+    var apiOptions = {
+      url: requestUrl,
+      token: parameters.token,
+      user: parameters.user,
+      payload: parameters.update,
+      method: 'POST'
+    };
+
+    if (parameters.odataParams !== undefined) {
+      apiOptions['query'] = parameters.odataParams;
+    }
+
+    base.makeApiCall(apiOptions, function(error, response) {
+      if (error) {
+        if (typeof callback === 'function') {
+          callback(error, response);
+        }
+      }
+      else if (response.statusCode !== 200) {
+        if (typeof callback === 'function') {
+          callback('REST request returned ' + response.statusCode + '; body: ' + JSON.stringify(response.body), response);
+        }
+      }
+      else {
+        if (typeof callback === 'function') {
+          callback(null, response.body);
+        }
+      }
+    });
+  },
+
+
 };
